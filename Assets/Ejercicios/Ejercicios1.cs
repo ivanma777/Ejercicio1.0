46fa577af91e6d2e960679c8fecf9074ab224e97
ivanma777 <ivamaruiz5604@gmail.com>
2024-09-12 17:55:21 +0200

    ejer1

--- Assets/Ejercicios/Ejercicios1.cs
+++ Assets/Ejercicios/Ejercicios1.cs
@@ -7,12 +7,18 @@ public class Ejercicios1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hola Mundo!");
+        Debug.Log("Este es el primer videojuego de Ivan");
+        Debug.Log("Estoy aprendiendo C#");
+        Debug.Log("Estoy aprendiendo C#");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Ha pasado un frame");
+
     }
+    //debido a que desde el start es solo una vez mientras que en el update es debido a que se repite cada frame//
 }
