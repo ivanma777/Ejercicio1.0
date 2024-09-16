e0b5f681916d4c507290948138a05a11ccdb8df3
ivanma777 <ivamaruiz5604@gmail.com>
2024-09-16 12:47:53 +0200

    ejercicio2-8

---
+++ Assets/Ejercicios/Ejercicio2_7.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_7 : MonoBehaviour
+{
+    int oro = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oro = 7 * 60 * 60 * 4;
+        
+        Debug.log("En cuatro horas tienes " +  oro + " de oro");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ejercicios/Ejercicio2_8.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_8 : MonoBehaviour
+{
+    int vida1 = 120;
+    float veneno = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(VenenoQuita);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VenenoQuita();
+    }
+
+
+    void VenenoQuita(veneno < 0,15)
+    {
+        veneno += 0,03;
+        vida1 -= vida1 * veneno;
+
+        Debug.log(vida1);
+    
+    }
+
+}
