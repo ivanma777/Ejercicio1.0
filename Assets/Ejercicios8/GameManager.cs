8f9e81087d71b5c96bd99a08143fe259a988ff57
ivanma777 <ivamaruiz5604@gmail.com>
2024-09-29 17:07:55 +0200

    ejercicio8_1

--- Assets/Ejercicios8/GameManager.cs
+++ Assets/Ejercicios8/GameManager.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] Personaje1 personaje1;
+    [SerializeField] Personaje1 personaje2;
+    int yaEjecutado = 1;
+
     // Start is called before the first frame update
     void Start()
     {
+        if (yaEjecutado == 1)
+        {
+            PrepararPersonajes();
+            Debug.Log("a");
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                
+                
+                Debug.Log(personaje1.Vida + "y exp " + personaje1.Exp + " y LVL" + personaje1.CalcularNivel(personaje1.Exp));
+                Debug.Log(personaje2.Vida + "y exp " + personaje2.Exp + " y LVL" + personaje2.CalcularNivel(personaje2.Exp));
+
+                yaEjecutado ++;
+            }
+
+
+        }
         
+
+
     }
 
     // Update is called once per frame
-    void Update()
+    void PrepararPersonajes( )
     {
-        
+        personaje1.Vida = 40;
+        personaje2.Vida = 40;
+        personaje1.Exp = 40;
+        personaje2.Exp = 40;
+
+       
+       
+
+       
+       
     }
 }
--- Assets/Ejercicios8/Personaje1.cs
+++ Assets/Ejercicios8/Personaje1.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class Personaje1 : MonoBehaviour
 {
+    
     private string nombre;
     private int exp;
     private int vida;
+    
+
+    public int Vida { get => vida; set => vida = value; }
+    public int Exp { get => exp; set => exp = value; }
+   
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-   float CacularNivel()
-    {
-        int LVL;
-        LVL = exp / 1000;
-        return LVL;
+   public float CalcularNivel(int exp)
+          {
+                int LVL;
+                LVL = exp / 1000;
+                return LVL;
 
-    }
+          }
 }
